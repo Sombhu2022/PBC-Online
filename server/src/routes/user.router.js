ff029a96e1bc486b3b2bea19e377791ed291bf0b
sourav6251 <dassourav3738+github@gmail.com>
2025-04-22 17:04:57 +0530

    Sourav

--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,19 +1,17 @@
 import express from "express";
 import { validate } from "../middlewares/validate.middleware.js";
-// import { userSchemas } from "../validations/user.valiation.js"
-// import userController, { createUser, logInUser, sendOtpForVerifyAccount, VerifyOtpWithExpiry } from "../controller/user.controller.js"
-import userValidation from "../validations/user.validation.js";
-import userController from "../controller/user.controller.js";
+import { userSchemas } from "../validations/user.valiation.js"
+import userController, { createUser, logInUser, sendOtpForVerifyAccount, VerifyOtpWithExpiry } from "../controller/user.controller.js"
+
 
 const router = express.Router();
 
 router
-    .post("/create", validate(userValidation.create), userController.createUser)
-    .get("/", userController.showAllUser);
-    // .post("/create", validate(userSchemas.createUser),  createUser)
-    // .post("/login", validate(userSchemas.login),  logInUser)
-    // .post("/send-otp", validate(userSchemas.sendOtp),  sendOtpForVerifyAccount)
-    // .post("/verify-otp", validate(userSchemas.verifyOtp),  VerifyOtpWithExpiry)
+
+    .post("/create", validate(userSchemas.createUser),  createUser)
+    .post("/login", validate(userSchemas.login),  logInUser)
+    .post("/send-otp", validate(userSchemas.sendOtp),  sendOtpForVerifyAccount)
+    .post("/verify-otp", validate(userSchemas.verifyOtp),  VerifyOtpWithExpiry)
     // .patch("/change-profile-pic", validate(userSchemas.changeProfilePic),  changeProfilePic)
     // .patch("/update-role/:userId", validate(userSchemas.updateUserRole),  updateUserRoleByAdmin)
     // .patch("/two-step-auth", validate(userSchemas.addTwoStepVerification),  addTwoStepVerification)
